048f7d7f220c5a5f4b1385c8e0cf4852bf34538f
Trevor Chan <trev@trevrosa.dev>
2026-07-08 15:30:37 +0800

    sync.sh: add udev and polkit dirs

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,4 +3,6 @@
 dir=$(dirname $0)
 
 rsync --delete -avL $@ --filter="merge $dir/config.filter" ~/.config/ ~/.dotfiles/.config
-rsync --delete -avL $@ --filter="merge $dir/user.filter" ~/ ~/.dotfiles/
+rsync --delete -avL $@ --filter="merge $dir/user.filter" ~/ ~/.dotfiles
+rsync --delete -avL $@ /etc/udev/rules.d/ ~/.dotfiles/udev
+rsync --delete -avL $@ /etc/polkit-1/rules.d/ ~/.dotfiles/polkit
